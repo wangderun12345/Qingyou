34b20402c22951f62cad9bf944f03e16850f0620
wangderun12345 <727075670@qq.com>
2019-06-12 15:35:50 +0800

    Create Chakanjipiao.java

---
+++ Chakanjipiao.java
@@ -0,0 +1,31 @@
+package com.example.appiumdemo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import io.appium.java_client.android.AndroidDriver;
+
+public class Chakanjipiao  {
+    AndroidDriver<WebElement> driver;
+    public void Chakanjipiao() throws InterruptedException {
+        driver.findElementByAndroidUIAutomator
+                ("new UiSelector().text(\"行程\")").click();
+        driver.findElementByAndroidUIAutomator
+                ("new UiSelector().text(\"添加行程\")").click();
+        driver.findElementByAndroidUIAutomator
+                ("new UiSelector().text(\"机票\")").click();
+        driver.findElementByAndroidUIAutomator
+                ("new UiSelector().text(\"查找\")").click();
+        driver.currentActivity();
+        System.out.println();
+    }
+
+
+}
